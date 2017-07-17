6b3e9038aa23d58899e3d68b4e881262ef5490d6
lazywithclass <montecristoh@gmail.com>
2017-07-17 22:12:13 +0200

    dont add an item if its not in the basket

--- scripts/generate-receipt.js
+++ scripts/generate-receipt.js
@@ -8,14 +8,33 @@ let compiled = handlebars.compile(template);
 let basket = require('../basket.json')
 
 let data = {
-  items: [
-    { item: 'Apples', price: prices.simpleAddition(basket, 'apple') },
-    { item: 'Bananas', price: prices.simpleAddition(basket, 'banana') },
-    { item: 'Oranges', price: prices.simpleAddition(basket, 'orange') },
-    { item: 'Papayas', price: prices.simpleAddition(basket, 'papaya') }
-  ],
+  items: [],
   generatedOn: moment().format('MMMM DD, YYYY'),
   total: prices.basketAddition(basket)
+}
+
+if (basket.indexOf('apple') > -1) {
+  data.items.push({
+    item: 'Apples', price: prices.simpleAddition(basket, 'apple')
+  })
+}
+
+if (basket.indexOf('banana') > -1) {
+  data.items.push({
+    item: 'Bananas', price: prices.simpleAddition(basket, 'banana')
+  })
+}
+
+if (basket.indexOf('orange') > -1) {
+  data.items.push({
+    item: 'Oranges', price: prices.simpleAddition(basket, 'orange')
+  })
+}
+
+if (basket.indexOf('papaya') > -1) {
+  data.items.push({
+    item: 'Papayas', price: prices.threeForTwoAddition(basket, 'papaya')
+  })
 }
 
 fs.writeFile('./receipt.html', compiled(data), function(err) {
