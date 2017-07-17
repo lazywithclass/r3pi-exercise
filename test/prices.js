bdffc8fad32e19e0bbace6b2edcd23c28f089620
lazywithclass <montecristoh@gmail.com>
2017-07-17 17:25:53 +0200

    inverted order of parameters for prices functions so that the basket could be partially applied later on

--- test/prices.js
+++ test/prices.js
@@ -14,13 +14,13 @@ describe('prices', () => {
   describe('simpleAddition', () => {
 
     it('adds items of the same type, given a price list', () => {
-      prices.simpleAddition('apple', basket).should.equal(25)
-      prices.simpleAddition('orange', basket).should.equal(30)
-      prices.simpleAddition('banana', basket).should.equal(30)
+      prices.simpleAddition(basket, 'apple').should.equal(25)
+      prices.simpleAddition(basket, 'orange').should.equal(30)
+      prices.simpleAddition(basket, 'banana').should.equal(30)
     })
 
     it('gives 0 if there are no items of the given type', () => {
-      prices.simpleAddition('mango', basket).should.equal(0)
+      prices.simpleAddition(basket, 'mango').should.equal(0)
     })
 
   })
@@ -28,12 +28,11 @@ describe('prices', () => {
   describe('threeForTwoAddition', () => {
 
     it('adds items of the same type, applying a 3x2 discount', () => {
-      prices.threeForTwoAddition('papaya', basket).should.equal(100)
-      
+      prices.threeForTwoAddition(basket, 'papaya').should.equal(100)
     })
 
     it('gives 0 if there are no items of the given type', () => {
-      prices.threeForTwoAddition('mango', basket).should.equal(0)
+      prices.threeForTwoAddition(basket, 'mango').should.equal(0)
     })
 
   })
